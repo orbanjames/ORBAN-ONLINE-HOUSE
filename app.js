f051e3d5678059a6d0056451abcac368b13598a5
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-21 01:42:49 +0200

    products class commit

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ const productsDOM = document.querySelector('.products-center')
 let cart = [];
 
 //getting the products
-class Products{}
+class Products{
+ async getProducts(){
+  try {
+   let result = await fetch('products.json')
+   let data = await result.json()
+   let products = data.items;
+   products = products.map(item =>{
+    const {title, price} = item.fields
+    const {id} = item.sys
+    const image = item.fields.image.fields.file.url
+    return {title, price, id, image}
+   })
+   return products
+  } catch (error) {
+   console.log(error);
+  }
+ }
+}
 
 //display products
 class UI {}
@@ -25,5 +42,8 @@ class Storage{}
 document.addEventListener("DOMContentLoaded", () => {
 const ui = new UI();
 const products = new Products();
+
+//get all products
+products.getProducts().then(products => console.log(products));
 
 });
