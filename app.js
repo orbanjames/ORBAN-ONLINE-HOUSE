a8979610debded2673cde2333f7854c0df3d08e3
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-21 03:17:00 +0200

    subsequent addition

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const productsDOM = document.querySelector('.products-center')
 
 //cart
 let cart = [];
+//buttons
+let buttonsDOM = []
 
 //getting the products
 class Products{
@@ -56,10 +58,25 @@ class UI {
   productsDOM.innerHTML = result; 
   }
   getBagButtons(){
-   const buttons = [...document.querySelectorAll('.bag-btn')] 
+   const buttons = [...document.querySelectorAll('.bag-btn')]
+   buttonsDOM = buttons ;
    buttons.forEach(button =>{
    let id = button.dataset.id;
    let inCart = cart.find(item =>item.id === id);
+   if(inCart){
+    button.innerText = 'In Cart'
+    button.disabled = true;
+   }
+    button.addEventListener('click', (event)=>{
+      event.target.innerText = 'In Cart'
+      event.target.disabled = true
+      //get product from products
+      //add product to the cart
+      //save cart in local storage
+      //set cart values
+      //display cart item
+      //show the cart
+    })
    })
    
    
