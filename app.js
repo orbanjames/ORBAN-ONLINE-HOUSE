d7dcbeb61fb0cdc57f5249d63637daf316d7d931
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-21 05:47:19 +0200

    subsequent commit and update

--- app.js
+++ app.js
@@ -126,6 +126,23 @@ class UI {
    cartDOM.classList.add('showCart')
 
   }
+  setupAPP(){
+ cart = Storage.getCart();
+ this.setCartValues(cart);
+ this.populateCart(cart);
+ cartBtn.addEventListener('click', this.showCart)
+ closeCartBtn.addEventListener('click', this.hideCart)
+  }
+  populateCart(cart){
+   cart.forEach(item => this.addCartItem(item))
+  }
+ hideCart(){
+  cartOverlay.classList.remove('transparentBcg')
+  cartDOM.classList.remove('showCart')
+ } 
+ cartLogic(){
+  
+ }
 }
 
 //local storage
@@ -140,17 +157,23 @@ class Storage{
  static saveCart(cart){
   localStorage.setItem('cart', JSON.stringify(cart))
  }
+ static getCart(){
+  return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[]
+ }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 const ui = new UI();
 const products = new Products();
+//setup app
+ui.setupAPP();
 
 //get all products
 products.getProducts().then(products =>{ ui.displayProducts(products)
 Storage.saveProducts(products)
 }).then(()=>{
 ui.getBagButtons()
+ui.cartLogic()
 })
 
 });
