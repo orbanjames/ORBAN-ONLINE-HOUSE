c83544a9c8d9e85f4c9f510200bf182dd5b63db0
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-21 04:30:26 +0200

    subsequent commits and updates

--- app.js
+++ app.js
@@ -71,22 +71,45 @@ class UI {
       event.target.innerText = 'In Cart'
       event.target.disabled = true
       //get product from products
+      let cartItem = {...Storage.getProduct(id), amount: 1}
       //add product to the cart
+      cart = [...cart, cartItem]
       //save cart in local storage
+      Storage.saveCart(cart);
       //set cart values
+      this.setCartValues(cart)
       //display cart item
+      this.addCartItem(cartItem)
       //show the cart
     })
-   })
-   
-   
+   }) 
   }
+  setCartValues(cart){
+   let tempTotal = 0;
+   let itemsTotal = 0;
+   cart.map(item =>{
+    tempTotal += item.price * item.amount;
+    itemsTotal += item.amount
+   })
+   cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+   cartItems.innerText = itemsTotal;
+  }
+ addCartItem(item){
+
+ }
 }
 
 //local storage
 class Storage{
  static saveProducts(products){
   localStorage.setItem('products', JSON.stringify(products))
+ }
+ static getProduct(id){
+  let products = JSON.parse(localStorage.getItem('products'))
+  return products.find(product => product.id === id)
+ }
+ static saveCart(cart){
+  localStorage.setItem('cart', JSON.stringify(cart))
  }
 }
 
